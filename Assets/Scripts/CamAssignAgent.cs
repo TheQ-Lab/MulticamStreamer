af2f2711d41ea91faf290886f84498fa5f31a72e
TheQ-Lab <73586510+TheQ-Lab@users.noreply.github.com>
2024-05-13 04:10:12 +0200

    mini fixes
    -no menu
    more other small things

--- Assets/Scripts/CamAssignAgent.cs
+++ Assets/Scripts/CamAssignAgent.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using MBExtensions;
-using UnityEditor.ShaderGraph.Internal;
 
 public class CamAssignAgent: MonoBehaviour
 {
